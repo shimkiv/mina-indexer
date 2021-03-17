e0972ccbf29d93f6cf57ba23817207f06ffae4e1
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-03-17 10:02:35 -0500

    Fix delegations count calculation query

--- store/queries/validators_update_staking.sql
+++ store/queries/validators_update_staking.sql
@@ -1,5 +1,8 @@
 WITH staking AS (
-  SELECT delegate, SUM(balance) AS total, COUNT(1) AS delegations
+  SELECT
+    delegate,
+    SUM(balance) AS total,
+    COUNT(1) FILTER (WHERE delegation IS TRUE) AS delegations
   FROM ledger_entries
   WHERE ledger_id = (SELECT id FROM ledgers ORDER BY id DESC LIMIT 1)
   GROUP BY delegate
