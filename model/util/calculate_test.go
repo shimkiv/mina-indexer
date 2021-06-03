6a7b9105b0896b401f132faae445507cbd2dd7e7
eesmerdag <emiresmerdag@gmail.com>
2021-06-03 03:09:30 +0300

    case for locked balance

--- model/util/calculate_test.go
+++ model/util/calculate_test.go
@@ -180,4 +180,101 @@ func TestCalculateSuperchargedWeighting(t *testing.T) {
 	}
 }
 
-// TODO: unit test for setting heights
+func TestCalculateSuperchargedContribution(t *testing.T) {
+	type args struct {
+		superchargedWeighting types.Percentage
+		timedWeighting        types.Percentage
+	}
+	tests := []struct {
+		name    string
+		args    args
+		result  types.Percentage
+		wantErr bool
+	}{
+		{
+			name: "successful locked entire epoch",
+			args: args{
+				superchargedWeighting: types.NewPercentage("1.9756"),
+				timedWeighting:        types.NewPercentage("0"),
+			},
+			result: types.NewPercentage("1"),
+		},
+		{
+			name: "successful unlocked entire epoch",
+			args: args{
+				superchargedWeighting: types.NewPercentage("1.9756"),
+				timedWeighting:        types.NewPercentage("1"),
+			},
+			result: types.NewPercentage("1.9756"),
+		},
+		{
+			name: "successful changed during epoch",
+			args: args{
+				superchargedWeighting: types.NewPercentage("1.9756"),
+				timedWeighting:        types.NewPercentage("1.5"),
+			},
+			result: types.NewPercentage("2.4634"),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := CalculateSuperchargedContribution(tt.args.superchargedWeighting, tt.args.timedWeighting)
+			if err != nil {
+				assert.True(t, tt.wantErr)
+			} else {
+				assert.Equal(t, res.String(), tt.result.String())
+			}
+		})
+	}
+}
+
+func TestCalculateWeightsSupercharged(t *testing.T) {
+	type args struct {
+		superchargedContribution types.Percentage
+		records                  []model.LedgerEntry
+	}
+	tests := []struct {
+		name    string
+		args    args
+		result  []string
+		wantErr bool
+	}{
+		{
+			name: "successful locked entire epoch",
+			args: args{
+				superchargedContribution: types.NewPercentage("1.98765"),
+				records: []model.LedgerEntry{
+					{
+						PublicKey:    "one",
+						Balance:      types.NewAmount("20000"),
+						LockedTokens: types.NewAmount("0"),
+					},
+					{
+						PublicKey:    "two",
+						Balance:      types.NewAmount("50000"),
+						LockedTokens: types.NewAmount("0"),
+					},
+					{
+						PublicKey:    "three",
+						Balance:      types.NewAmount("30000"),
+						LockedTokens: types.NewAmount("1"),
+					},
+				},
+			},
+			result: []string{"0.2350364057", "0.5875910143", "0.17737258"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			records := tt.args.records
+			err := CalculateWeightsSupercharged(tt.args.superchargedContribution, records)
+			if err != nil {
+				assert.True(t, tt.wantErr)
+			} else {
+				for i, _ := range records {
+					assert.Equal(t, tt.result[i], records[i].Weight.String())
+				}
+			}
+		})
+	}
+}
