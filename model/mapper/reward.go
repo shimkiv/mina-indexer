ab9c9e1b3bd557d972bb1ca5ebfcca482ccc3f2b
eesmerdag <emiresmerdag@gmail.com>
2021-06-04 17:12:28 +0300

    reward coinbaserewards field

--- model/mapper/reward.go
+++ model/mapper/reward.go
@@ -49,13 +49,6 @@ func FindValidatorFee(validatorEpochs []model.ValidatorEpoch, creator string) (t
 	return types.Percentage{}, errors.New("validator fee not found")
 }
 
-func CoinbaseReward(block *graph.Block) types.Amount {
-	if block.Transactions != nil {
-		return types.NewAmount(block.Transactions.Coinbase)
-	}
-	return types.NewAmount("")
-}
-
 func TransactionFees(block *graph.Block) types.Amount {
 	res := types.NewInt64Amount(0)
 	if block.Transactions != nil && block.Transactions.FeeTransfer != nil {
