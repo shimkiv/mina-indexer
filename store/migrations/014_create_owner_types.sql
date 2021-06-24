4886f201d73db50cb24b01c5b4ab92c53c19569b
eesmerdag <emiresmerdag@gmail.com>
2021-06-24 18:14:36 +0300

    drop type

--- store/migrations/014_create_owner_types.sql
+++ store/migrations/014_create_owner_types.sql
@@ -1,9 +1,5 @@
 -- +goose Up
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TYPE OWNER_TYPE AS ENUM ('validator', 'delegator', 'unknown');
 
 -- +goose Down
-DROP EXTENSION IF EXISTS "uuid-ossp";
-
-DROP DOMAIN IF EXISTS OWNER_TYPE;
+DROP TYPE IF EXISTS OWNER_TYPE;
