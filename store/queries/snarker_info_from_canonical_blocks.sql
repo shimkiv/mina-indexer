27fd431bdbac3edaea211e2a4996f7c8878d7c97
eesmerdag <emiresmerdag@gmail.com>
2021-07-09 18:54:23 +0300

    formatting

--- store/queries/snarker_info_from_canonical_blocks.sql
+++ store/queries/snarker_info_from_canonical_blocks.sql
@@ -1,11 +1,11 @@
 SELECT
- count(*) as jobs_count,
- sum(works_count) as work_count
+  count(*) as jobs_count,
+  sum(works_count) as work_count
 FROM
- snark_jobs
+  snark_jobs
 WHERE
- prover = $1
- AND block_hash IN
+  prover = $1
+  AND block_hash IN
         (SELECT hash FROM blocks
         WHERE height >= $2 AND height <= $3 AND canonical = TRUE
         AND $1 = ANY(snarker_accounts))
