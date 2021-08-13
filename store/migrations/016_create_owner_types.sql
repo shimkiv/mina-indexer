71d137cee12dca1f4d57f171bbb07ec92af2d097
eesmerdag <emiresmerdag@gmail.com>
2021-08-13 20:19:51 +0300

    remove unnecessary owner type

--- store/migrations/016_create_owner_types.sql
+++ store/migrations/016_create_owner_types.sql
@@ -1,5 +1,5 @@
 -- +goose Up
-CREATE TYPE OWNER_TYPE AS ENUM ('validator', 'delegator', 'unknown');
+CREATE TYPE OWNER_TYPE AS ENUM ('validator', 'delegator');
 
 -- +goose Down
 DROP TYPE IF EXISTS OWNER_TYPE;
