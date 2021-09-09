870b1cd4b8fd28f5cd5438f838740af229647936
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-09-09 17:55:35 -0500

    Expose validator fee in the validators list endpoint

--- store/queries/validators_index.sql
+++ store/queries/validators_index.sql
@@ -1,3 +1,8 @@
+WITH validator_fees AS (
+  SELECT DISTINCT ON(account_address) account_address, validator_fee
+  FROM validator_epochs
+  ORDER BY account_address, epoch DESC
+)
 SELECT
   validators.public_key,
   validators.identity_name,
@@ -5,16 +10,18 @@ SELECT
   validators.start_time,
   validators.last_height,
   validators.last_time,
-  COALESCE(validators.stake::TEXT) AS stake,
   validators.blocks_created,
   validators.blocks_proposed,
   validators.delegations,
   COALESCE(validators.stake, 0)::TEXT AS stake,
   COALESCE(accounts.balance, 0)::TEXT AS account_balance,
-  COALESCE(accounts.balance_unknown, 0)::TEXT AS account_balance_unknown
+  COALESCE(accounts.balance_unknown, 0)::TEXT AS account_balance_unknown,
+  validator_fees.validator_fee AS fee
 FROM
   validators
 LEFT JOIN accounts
   ON accounts.public_key = validators.public_key
+LEFT JOIN validator_fees
+  ON validator_fees.account_address = validators.public_key
 ORDER BY
   blocks_created DESC
