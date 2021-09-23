f3f448ba1f122d1aaadfbd9b0ea8daeabc103073
Dan Sosedoff <dan.sosedoff@gmail.com>
2021-09-23 10:42:23 -0500

    Fix down migration

--- store/migrations/023_remove_staketab_id_from_validator_epochs.sql
+++ store/migrations/023_remove_staketab_id_from_validator_epochs.sql
@@ -7,6 +7,10 @@ CREATE UNIQUE INDEX idx_validator_epochs_account_epoch
 ALTER TABLE validator_epochs DROP COLUMN staketab_id;
 
 -- +goose Down
+ALTER TABLE validator_epochs ADD COLUMN staketab_id INTEGER;
+
+DROP INDEX idx_validator_epochs_account_epoch;
+
 CREATE UNIQUE INDEX idx_validator_epochs_account_epoch
   ON validator_epochs(staketab_id, epoch);
 
